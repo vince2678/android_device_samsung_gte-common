b4beeee531a09024ae9ba08ad9f55f621cec1331
Sean Hoyt <seanhoyt963@gmail.com>
2018-04-18 04:04:57 -0400

    Add the gt5 family to the vendor barrier
    Change-Id: I1c7c5a8284e3a3b960d598faf85dd663adb613a4

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -19,7 +19,7 @@
 set -e
 
 # Required!
-export DEVICES="gtelwifiue gtesqltespr gt510wifi"
+export DEVICES="gtelwifiue gtesqltespr gt510wifi gt510lte gt58wifi gt58lte"
 export DEVICE_COMMON=gte-common
 export BOARD_COMMON=msm8916-common
 export VENDOR=samsung
